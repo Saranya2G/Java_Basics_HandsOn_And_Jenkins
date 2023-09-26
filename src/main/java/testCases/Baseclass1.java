8efcdc178a7dfbfabe44f5cf0c9e10dcdc4037d6
unknown <saranyaganesana@gmail.com>
2023-09-26 16:18:13 +0530

    added print statement

--- src/main/java/testCases/Baseclass1.java
+++ src/main/java/testCases/Baseclass1.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Baseclass1 {
 	
-	String Path = "C:\\Users\\Aaludra_05\\Downloads\\Demoproject (1)\\Demoproject\\Chromedriver\\chromedriver.exe";
+	String Path = "C:\\Users\\Aaludra_05\\Documents\\selenium_jenkins_demo\\Java_Basics_HandsOn_And_Jenkins\\Chromedriver\\chromedriver.exe";
 	String url = "https://www.saucedemo.com/?ref=hackernoon.com";
 	public static WebDriver driver;
 	
@@ -20,7 +20,9 @@ public class Baseclass1 {
 	@BeforeSuite
 	public void getin() {
 		System.setProperty("webdriver.chrome.driver", Path);
+		System.out.println("Chrome driver was started sucessfully");
 		driver = new ChromeDriver();
+		System.out.println("Chrome driver was started sucessfully");
 		driver.manage().window().maximize();
 		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(url);
